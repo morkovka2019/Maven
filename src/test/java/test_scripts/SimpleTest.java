42de63b488fee0921b0bd66b15ca685ca933bbe1
maryanapozina <morkovka2017@yahoo.com>
2022-02-12 12:41:37 +0200

    first commit

---
+++ src/test/java/test_scripts/SimpleTest.java
@@ -0,0 +1,12 @@
+package test_scripts;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+        @Test
+        public void sumTest() {
+            Assert.assertEquals(7, 10 - 3);
+        }
+
+}
